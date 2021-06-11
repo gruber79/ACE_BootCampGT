ca52f4d606175c13fd47864b2d7530357f1ef8c5
Galo Toba <84482349+gruber79@users.noreply.github.com>
2021-06-11 08:09:40 +0000

    I create lab14

---
+++ labs/labs12/lab12.js
@@ -0,0 +1,19 @@
+print("Lets calculate the circumference")
+import math
+r=int(input("What is the radius of your cylinder?"))
+h=int(input("What is the height of your cylinder?"))
+v=math.pi*((r**2)*h)
+print("volume of the circle=", v)
+#Quadratic formula program.
+name=input("What is your name")
+print("Hello" + " " + name)
+print("Now let the program calculate the quadratic")
+import math  
+a = float(input('Enter coefficient a: '))  
+b = float(input('Enter coefficient b: '))  
+c = float(input('Enter coefficient c: '))  
+d = (b**2) - (4*a*c)  
+sol1=(-b-math.sqrt(c))/(2*a)  
+sol2=(-b+math.sqrt(c))/(2*a)  
+print("result 1=", sol1)
+print("result 2=", sol2)  
