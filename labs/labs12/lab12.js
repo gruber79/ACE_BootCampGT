e0d3a12c9c67affcc1c1b32d35ca9b73bc6a0734
Galo Toba <84482349+gruber79@users.noreply.github.com>
2021-06-12 06:03:22 +0000

    I create lab12

--- labs/labs12/lab12.js
+++ labs/labs12/lab12.js
@@ -1,19 +1,15 @@
-print("Lets calculate the circumference")
-import math
-r=int(input("What is the radius of your cylinder?"))
-h=int(input("What is the height of your cylinder?"))
-v=math.pi*((r**2)*h)
-print("volume of the circle=", v)
-#Quadratic formula program.
-name=input("What is your name")
-print("Hello" + " " + name)
-print("Now let the program calculate the quadratic")
-import math  
-a = float(input('Enter coefficient a: '))  
-b = float(input('Enter coefficient b: '))  
-c = float(input('Enter coefficient c: '))  
-d = (b**2) - (4*a*c)  
-sol1=(-b-math.sqrt(c))/(2*a)  
-sol2=(-b+math.sqrt(c))/(2*a)  
-print("result 1=", sol1)
-print("result 2=", sol2)  
+
+function calculate(){ 
+var r, h, valume ; 
+var r = document.getElementById("text").value; 
+var h = document.getElementById("text1").value;
+var volume = 5; 
+document.getElementById("vol").innerHTML = 'The cylinder valume is: ' + volume;
+document.getElementById("vol1").innerHTML = '';
+}
+
+
+
+
+
+
