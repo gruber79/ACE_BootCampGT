b38d8acc72524d2dd207fc7c3668e65f0d48443d
Galo Toba <84482349+gruber79@users.noreply.github.com>
2021-06-14 05:50:18 +0000

    final update

---
+++ labs/labs14/lab14.js
@@ -0,0 +1,10 @@
+var z = new Date().getFullYear();
+function year(){
+  var edad,birth;
+  var birth = document.getElementById("text").value;
+  var edad = (z - birth);
+  if (birth > 1900){
+    document.getElementById("val").innerHTML = '<big>I will be ' + (edad+4) + ' or ' + (edad+5) + ' years old in july <strong>' + (z+5) + '</strong></big>';
+    document.getElementById("valo").innerHTML = '';   
+}
+}
--- projects/project2/fortu.js
+++ projects/project2/fortu.js
@@ -1,5 +1,6 @@
+
 function change() {
-  document.getElementById('pop').innerHtml='Hey Band!! How are you?';
+  document.getElementById("pop").innerHtml='Hey Band!! How are you?';
 }     
 function cssChange() {
   document.getElementById(;'sty').innerHtml= '<link rel="stylesheet" type="text/css" href="style1.css">';
@@ -13,4 +14,13 @@ function cssChange2() {
 }
 function cssChange3() {
   document.getElementById('sty').innerHtml='<link rel="stylesheet" type="text/css" href="style4.css">';
+}
+  function.changen() {
+  document.getElementById("pop").innerHTML = 'Hey Band!! How are you?';
+}
+function myFunction() {
+  var x = document.getElementById("myText").value;
+  var y = document.getElementById("myText1").value;
+  var future = 'Hello '+ '  ' + x + '  ' + y + ' nice to meet you.';
+   document.getElementById('InfData').innerHTML = future;
 }
