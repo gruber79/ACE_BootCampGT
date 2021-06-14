fd1533b8f6b257cc05ecb4fcd098c3216dc7d884
Galo Toba <84482349+gruber79@users.noreply.github.com>
2021-06-14 07:02:39 +0000

    I create JScript for lab14 because was delete for accident

--- labs/labs14/lab14.js
+++ labs/labs14/lab14.js
@@ -1,10 +1,16 @@
-var z = new Date().getFullYear();
-function year(){
-  var edad,birth;
-  var birth = document.getElementById("text").value;
-  var edad = (z - birth);
-  if (birth > 1900){
-    document.getElementById("val").innerHTML = '<big>I will be ' + (edad+4) + ' or ' + (edad+5) + ' years old in july <strong>' + (z+5) + '</strong></big>';
-    document.getElementById("valo").innerHTML = '';   
-}
+
+function cash_register() {
+  var a, b, c, d, e, f, g, h, x;
+  var a = document.getElementById("text").value;
+  var b = document.getElementById("text1").value;
+  var c = document.getElementById("text2").value;
+  var d = document.getElementById("text3").value;
+  var e = document.getElementById("text4").value;
+  var f = document.getElementById("text5").value;
+  var g = document.getElementById("text6").value;
+  var h = document.getElementById("text7").value;
+  
+  var x = (a*145)+(b*45)+(c*25)+(d*5)+(e*8)+(f*4)+(g*115)+(h*15);
+
+    document.getElementById("val").innerHTML = x;
 }
